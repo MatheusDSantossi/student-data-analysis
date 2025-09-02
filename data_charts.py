43fc2c8063cadc9037d56e0dd4f7db77327710c4
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-09-02 18:14:42 -0300

    more tests and a new line chart scaffold

--- data_charts.py
+++ data_charts.py
@@ -37,13 +37,27 @@ print("Aggregated totals by state:\n", students_by_state.head())
 print(students_by_state.to_list())
 
 # Plot bar chart using concrete arrays
-plt.figure(figsize=(12, 6))
-plt.bar(students_by_state.index.astype(str), students_by_state.values)
-plt.xlabel("State")
-plt.ylabel("Students (total)")
-plt.title("Total Students by State")
-plt.xticks(rotation=45, ha="right")
-plt.tight_layout()
+# plt.figure(figsize=(12, 6))
+# plt.bar(students_by_state.index.astype(str), students_by_state.values)
+# plt.xlabel("State")
+# plt.ylabel("Students (total)")
+# plt.title("Total Students by State")
+# plt.xticks(rotation=45, ha="right")
+# plt.tight_layout()
+# plt.show()
+
+# print(df)
+
+course_quantity_students = df.groupby("course")["students_quantity"].sum()
+
+print(course_quantity_students.sort_values(ascending=False))
+
+print("list: ", course_quantity_students.to_list())
+print("values: ", course_quantity_students.values)
+
+plt.plot()
+plt.xlabel(course_quantity_students.index.astype(str).drop_duplicates())
+plt.ylabel(course_quantity_students.values)
 plt.show()
 
 cursor.close()
